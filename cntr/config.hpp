584bd99ef673d04be2a6b7a5dc33f2b09eddb36c
Sami Laine <sami.v.laine@gmail.com>
2022-09-19 23:42:13 +0200

    Add old cntr config

---
+++ cntr/config.hpp
@@ -0,0 +1,11 @@
+cntr_captureInterval = 1;
+cntr_minPlayerCount = 1;
+cntr_endCaptureOnEndMission = true;
+cntr_endCaptureOnNoPlayers = true;
+cntr_usingAce = true;
+cntr_roleDescription = false;
+
+cntr_exportPath = "C:\server\cntr-data";
+
+cntr_debug = false;
+cntr_debugLogEvents = false;
